ac96bf898f758b8a01a4da6cf6cfdb1ca7b05bca
Ben Russell <thematrixeatsyou@gmail.com>
2018-12-07 22:28:15 +1300

    let there be 15bpp RDP rects

--- rdp.h
+++ rdp.h
@@ -94,12 +94,29 @@ void rdp_run_one_command(void) {
 				xl >>= 2;
 				yl >>= 2;
 				printf("Render %d,%d -> %d,%d\n", xl, yl, xh, yh);
-				for(int y = yl; y < yh; y++) {
-					uint32_t offs = (rdp_color_image_addr>>2);
-					offs += (rdp_color_image_width+1)*y;
-					for(int x = xl; x < xh; x++) {
-						ram[offs+x] = rdp_fill_color;
-					}
+				switch(rdp_color_image_size)
+				{
+					case 2: // 15bpp
+						// FIXME this isn't actually right AFAIK
+						xl >>= 1;
+						xh >>= 1;
+						for(int y = yl; y < yh; y++) {
+							uint32_t offs = (rdp_color_image_addr>>2);
+							offs += ((rdp_color_image_width+1)>>1)*y;
+							for(int x = xl; x < xh; x++) {
+								ram[offs+x] = rdp_fill_color;
+							}
+						}
+						break;
+					case 3: // 32bpp
+						for(int y = yl; y < yh; y++) {
+							uint32_t offs = (rdp_color_image_addr>>2);
+							offs += (rdp_color_image_width+1)*y;
+							for(int x = xl; x < xh; x++) {
+								ram[offs+x] = rdp_fill_color;
+							}
+						}
+						break;
 				}
 			}
 			rdp_cmd_len = 0;
